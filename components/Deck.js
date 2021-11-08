145eee1cae33b87318c867fa916ac1530431f3f5
Ralf Guth <rsguth@gmail.com>
2021-11-08 13:08:09 -0300

    chore: atualiza deck

--- components/Deck.js
+++ components/Deck.js
@@ -5,8 +5,8 @@ import styles from '../styles/Deck.module.css'
 const Deck = () => {
     return (
         <div className={styles.grid}>
-          <Card link='https://github.com/ralfguth' title='Repositório do GitHub' description='meus projetos pessoais e estudos' />
-          <Card link='https://www.linkedin.com/in/ralf-guth-7404a7161/' title='Linkedin' description='Experiencias e skills profissionais' />
+          <Card link='https://github.com/ralfguth' title='Repositórios GitHub' description='meus projetos pessoais e estudos' />
+          <Card link='https://www.linkedin.com/in/ralf-guth-7404a7161/' title='Pefil do Linkedin' description='Experiencias e skills profissionais' />
           <Card link='https://www.instagram.com/ralfguth_/' title='Instagram' description='aquele gostinho de fotolog' />
           <Card link='https://twitter.com/rsguth' title='Twitter' description='128 caracteres e tal' />
         </div>
