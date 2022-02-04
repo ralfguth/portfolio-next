0113f5ad388e9d7ca3423c0d813dc925a396b59e
Ralf Guth <rsguth@gmail.com>
2022-02-03 21:55:30 -0300

    remove cards

--- components/Deck.js
+++ components/Deck.js
@@ -7,8 +7,6 @@ const Deck = () => {
         <div className={styles.grid}>
           <Card link='https://github.com/ralfguth' title='Repositório do GitHub' description='meus projetos pessoais e estudos' />
           <Card link='https://www.linkedin.com/in/ralf-guth-7404a7161/' title='Linkedin' description='Experiencias e skills profissionais' />
-          <Card link='https://www.instagram.com/ralfguth_/' title='Instagram' description='aquele gostinho de fotolog' />
-          <Card link='https://twitter.com/rsguth' title='Twitter' description='128 caracteres e tal' />
         </div>
     )
 }
