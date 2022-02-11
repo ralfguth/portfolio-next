6c0bd8a6c12253c36fde4076deff83680d042a55
Ralf Guth <rsguth@gmail.com>
2022-02-11 14:59:56 -0300

    modifica cards

--- components/Card.js
+++ components/Card.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Link from 'next/link'
 import styles from '../styles/Card.module.css'
 
-const Card = ({link,title,description}) => {
+const Card = ({ link, title, description }) => {
     return (
         <Link href={link}>
             <a className={styles.card}>
-            <h3>{title} &rarr;</h3>
-            <p>{description}</p>
-          </a>
+                <div className={styles.container}>
+                    <h3>{title} &rarr;</h3>
+                    <p>{description}</p>
+                </div>
+            </a>
         </Link>
     )
 }
