6559d039365a90ce691ade429d4fc83ef35d2fbe
Ralf Guth <rsguth@gmail.com>
2022-10-18 21:09:55 -0300

    corrige conteudo

--- components/Deck.js
+++ components/Deck.js
@@ -6,8 +6,8 @@ const Deck = () => {
     return (
         <div className={styles.grid}>
         
-          <Card link='https://github.com/ralfguth' title='Repositório do GitHub' description='meus projetos pessoais e estudos' />
-          <Card link='https://www.linkedin.com/in/ralf-guth-7404a7161/' title='LinkedIn' description='Experiencias e skills profissionais' />
+          <Card link='https://github.com/ralfguth' title='Repositório do GitHub' description='Meus projetos pessoais e estudos' />
+          <Card link='https://www.linkedin.com/in/ralf-guth-7404a7161/' title='LinkedIn' description='Formação e experiência profissional' />
           <Card link='mailto:rsguth@gmail.com' title='E-mail' description='Entre em contato ;)' />
 
         </div>
